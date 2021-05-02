19c3bd8381e60b1b610e8624024ebc6751456f2a
05ivannieto <05ivannieto@gmail.com>
2021-05-03 00:52:10 +0200

    borrado periftech duplicado
    No se como ha ocurrido esto, soy tonto

--- periftech/periftech/src/main/java/es/code/urjc/periftech/repositories/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package es.code.urjc.periftech.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.code.urjc.periftech.models.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-
-}
--- periftech/periftech/src/main/java/es/code/urjc/periftech/services/PedidoService.java
+++
@@ -1,8 +0,0 @@
-package es.code.urjc.periftech.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PedidoService {
-
-}
