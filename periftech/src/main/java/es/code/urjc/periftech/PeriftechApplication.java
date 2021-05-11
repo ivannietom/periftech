8d010a242282741d119265784715419bbf2245df
Carlos Herce Nombela <c.herce.2018@alumnos.urjc.es>
2021-05-11 23:30:07 +0200

    Añadida dependencia y configuración de Hazelcast

--- periftech/src/main/java/es/code/urjc/periftech/PeriftechApplication.java
+++ periftech/src/main/java/es/code/urjc/periftech/PeriftechApplication.java
@@ -1,14 +1,21 @@
 package es.code.urjc.periftech;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 @EnableCaching
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class PeriftechApplication {
 
 	public static void main(String[] args) {
@@ -20,4 +27,13 @@ public class PeriftechApplication {
 		return new ConcurrentMapCacheManager("categorias");
 	}
 	
+	@Bean
+	 public Config config() {
+	 Config config = new Config();
+	 JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+	 joinConfig.getMulticastConfig().setEnabled(false);
+	 joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+	 return config;
+	 }
+
 }
